9aeaa546c6f02a33b55547fc9f3da5d19eda99fe
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-12-02 07:35:51 +0530

    Update loan.js

--- LoanCalculator/loan.js
+++ LoanCalculator/loan.js
@@ -53,4 +53,81 @@ th ,td {vertical-align: top;}
   <div id ="lenders"></div></td></tr>
 </table>
 
+<script>
+   "use strict";
+function calculate(){
+  // take up input
+  var amount = document.getElementById("amount");
+  var apr = document.getElementById("apr");
+  var years=document.getElementById("years");
+  var zipcode = document.getElementById("zipcode");
+  var payment=document.getElementById("payment");
+  var total = document.getElementById("total");
+ var totalinterest=document.getElementById("totalinterest");
+ var principal = parseFloat(amount.value);
+ var interest = parseFloat(apr.value)/100/12;
+ var payments = parseFloat(years.value)*12;
+ 
+ 
+ var x = Math.pow(1+interest ,payments);
+ var monthly = prinicpal*x*interest / (x-1);
+ 
+ if(isFinite(monthly)){
+  payment.innerHTML = monthly.toFixed(2);
+  total.innerHTML = (monthly*payments).toFized(2);
+  totalinterest.innerHTML=((monthly*payments)-principal).toFixed(2);
+  
+  save(amount.value , apr.value , years.value , zipcode.value);
+  try{
+   getLenders(amount.value , apr.value , years.value , zipcode.value);
+  }
+  catch(e){
+  }
+ }else
+  
+  payment.innerHTML = " " ;
+  total.innerHTML = "";
+  totalinterest.innerHTML="";
+  chart();
+}
+}
 
+function save(amount , apr , years,zipcode ){
+  if(window.localStorage){
+   localStorage.loan_amount = amount ;
+   localStorage.loan_apr = apr ;
+   localStorage.loan_years = years ;
+   localStorage.loan_zipcode = zipcode ;
+  }
+}
+
+// restore input feilds when document first loads 
+
+ window.onload = function(){
+  if(window.localStorage && localStorage.loan_amount ){
+    document.getElementById("amount").value = localStorage.loan_amount ; 
+   document.getElementById("apr").value = localStorage.loan_apr ; 
+   document.getElementById("years").value = localStorage.loan_years ; 
+   document.getElementById("zipcode").value = localStorage.loan_zipcode ; 
+  }
+ }; 
+
+
+function getLenders(amount ,apr ,years,zipcode ){
+  if(!windows.XMLHttpRequest) return;
+  var ad = document.getElementById("lenders");
+ if(!ad) return ; 
+ 
+}
+
+
+
+
+
+
+
+
+
+   
+  }
+}
